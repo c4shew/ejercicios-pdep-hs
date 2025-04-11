22bdb9e976f9473139ddb7a8aebd5842654c07f5
Constanza Bianco <constanzabianco@outlook.com>
2025-04-11 15:12:44 -0300

    en entorno esta andando

--- probandos.hs
+++ probandos.hs
@@ -1,3 +1,4 @@
+import Text.Read.Lex (Number)
 -- Probando
 
 aproboAlumno :: Int -> Bool
@@ -6,3 +7,8 @@ aproboAlumno nota = nota >= 6
 
 edadDumbledore :: Integer
 edadDumbledore = 120
+
+
+pesosADolares :: Float -> Float
+pesosADolares pesos = pesos / 1075.22 
+
